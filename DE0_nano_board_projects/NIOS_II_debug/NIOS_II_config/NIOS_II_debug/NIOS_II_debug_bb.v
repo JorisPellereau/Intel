40a6c3cc4ea3272004ff768c640b64fe1e28e468
Joris P <jor.pell@hotmail.fr>
2019-06-20 16:19:51 +0200

    up

--- DE0_nano_board_projects/NIOS_II_debug/NIOS_II_config/NIOS_II_debug/NIOS_II_debug_bb.v
+++ DE0_nano_board_projects/NIOS_II_debug/NIOS_II_config/NIOS_II_debug/NIOS_II_debug_bb.v
@@ -1,10 +1,16 @@
 
 module NIOS_II_debug (
 	clk_clk,
+	reset_reset_n,
+	pio_adc_cmd_external_connection_export,
 	pio_uart_data_external_connection_export,
-	reset_reset_n);	
+	pio_adc_data_external_connection_export,
+	pio_adc_channel_external_connection_export);	
 
 	input		clk_clk;
-	input	[7:0]	pio_uart_data_external_connection_export;
 	input		reset_reset_n;
+	output	[3:0]	pio_adc_cmd_external_connection_export;
+	input	[7:0]	pio_uart_data_external_connection_export;
+	input	[11:0]	pio_adc_data_external_connection_export;
+	input	[2:0]	pio_adc_channel_external_connection_export;
 endmodule
--- DE0_nano_board_projects/NIOS_II_debug/NIOS_II_config/NIOS_II_debug/NIOS_II_debug_inst.vhd
+++ DE0_nano_board_projects/NIOS_II_debug/NIOS_II_config/NIOS_II_debug/NIOS_II_debug_inst.vhd
@@ -1,15 +1,21 @@
 	component NIOS_II_debug is
 		port (
-			clk_clk                                  : in std_logic                    := 'X';             -- clk
-			pio_uart_data_external_connection_export : in std_logic_vector(7 downto 0) := (others => 'X'); -- export
-			reset_reset_n                            : in std_logic                    := 'X'              -- reset_n
+			clk_clk                                    : in  std_logic                     := 'X';             -- clk
+			reset_reset_n                              : in  std_logic                     := 'X';             -- reset_n
+			pio_adc_cmd_external_connection_export     : out std_logic_vector(3 downto 0);                     -- export
+			pio_uart_data_external_connection_export   : in  std_logic_vector(7 downto 0)  := (others => 'X'); -- export
+			pio_adc_data_external_connection_export    : in  std_logic_vector(11 downto 0) := (others => 'X'); -- export
+			pio_adc_channel_external_connection_export : in  std_logic_vector(2 downto 0)  := (others => 'X')  -- export
 		);
 	end component NIOS_II_debug;
 
 	u0 : component NIOS_II_debug
 		port map (
-			clk_clk                                  => CONNECTED_TO_clk_clk,                                  --                               clk.clk
-			pio_uart_data_external_connection_export => CONNECTED_TO_pio_uart_data_external_connection_export, -- pio_uart_data_external_connection.export
-			reset_reset_n                            => CONNECTED_TO_reset_reset_n                             --                             reset.reset_n
+			clk_clk                                    => CONNECTED_TO_clk_clk,                                    --                                 clk.clk
+			reset_reset_n                              => CONNECTED_TO_reset_reset_n,                              --                               reset.reset_n
+			pio_adc_cmd_external_connection_export     => CONNECTED_TO_pio_adc_cmd_external_connection_export,     --     pio_adc_cmd_external_connection.export
+			pio_uart_data_external_connection_export   => CONNECTED_TO_pio_uart_data_external_connection_export,   --   pio_uart_data_external_connection.export
+			pio_adc_data_external_connection_export    => CONNECTED_TO_pio_adc_data_external_connection_export,    --    pio_adc_data_external_connection.export
+			pio_adc_channel_external_connection_export => CONNECTED_TO_pio_adc_channel_external_connection_export  -- pio_adc_channel_external_connection.export
 		);
 
